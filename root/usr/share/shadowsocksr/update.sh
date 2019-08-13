0d286a8d85f9a70ed0c04fb30294706cb3064708
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2019-08-13 19:11:53 +0800

    Update update.sh

--- root/usr/share/shadowsocksr/update.sh
+++ root/usr/share/shadowsocksr/update.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 chnroute_data=$(wget -O- -t 3 -T 3 http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest)
 [ $? -eq 0 ] && {
     echo "$chnroute_data" | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > /tmp/china_ssr.txt
