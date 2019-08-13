4e41476c68f5c1238a8215b4252c76f8ba3909fc
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2019-08-13 18:03:17 +0800

    Update advanced.lua

--- luasrc/model/cbi/shadowsocksr/advanced.lua
+++ luasrc/model/cbi/shadowsocksr/advanced.lua
@@ -19,24 +19,26 @@ table.sort(key_table)
 
 m = Map(shadowsocksr)
 
-s = m:section(TypedSection, "global", translate("Server failsafe auto swith settings"))
+
+
+-- [[ haProxy ]]--
+
+s = m:section(TypedSection, "global_haproxy", translate("haProxy settings"))
 s.anonymous = true
 
-o = s:option(Flag, "monitor_enable", translate("Enable Process Deamon"))
+o = s:option(Flag, "admin_enable", translate("Enabling the Management Console"))
 o.rmempty = false
+o.default = 1
 
-o = s:option(Flag, "enable_switch", translate("Enable Auto Switch"))
-o.rmempty = false
-
-o = s:option(Value, "switch_time", translate("Switch check cycly(second)"))
+o = s:option(Value, "admin_port", translate("Service Port"))
 o.datatype = "uinteger"
-o:depends("enable_switch", "1")
-o.default = 3600
+o.default = 1111
 
-o = s:option(Value, "switch_timeout", translate("Check timout(second)"))
-o.datatype = "uinteger"
-o:depends("enable_switch", "1")
-o.default = 5
+o = s:option(Value, "admin_user", translate("User name"))
+o.default = "admin"
+
+o = s:option(Value, "admin_password", translate("Password"))
+o.default = "root"
 
 -- [[ SOCKS5 Proxy ]]--
 if nixio.fs.access("/usr/bin/ssr-local") then
@@ -54,6 +56,19 @@ o.datatype = "port"
 o.default = 1080
 o.rmempty = false
 
+-- [[ HTTP Proxy ]]--
+if nixio.fs.access("/usr/sbin/privoxy") then
+o = s:option(Flag, "http_enable", translate("Enable HTTP Proxy"))
+o.rmempty = false
+
+o = s:option(Value, "http_port", translate("HTTP Port"))
+o.datatype = "port"
+o.default = 1081
+o.rmempty = false
+
 end
+end
+
+
 
 return m
